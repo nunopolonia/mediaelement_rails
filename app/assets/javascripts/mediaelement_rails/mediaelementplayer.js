e43157aee00c05f63e5acc61e6f86825888f8c38
Nuno Polonia <nunopolonia@gmail.com>
2014-10-21 11:55:53 +0100

    Last control always out of the bar by one pixel

--- app/assets/javascripts/mediaelement_rails/mediaelementplayer.js
+++ app/assets/javascripts/mediaelement_rails/mediaelementplayer.js
@@ -864,7 +864,7 @@ if (typeof jQuery != 'undefined') {
 				});
 
 				// fit the rail into the remaining space
-				railWidth = t.controls.width() - usedWidth - (rail.outerWidth(true) - rail.width());
+				railWidth = t.controls.width() - usedWidth - (rail.outerWidth(true) - rail.width()) - 1;
 			}
 			
 			// resize the rail,
